b74847e5f1247b9e71b36b1c817f43f71b02925d
Gilberto <gil.martinez.meji@gmail.com>
2026-04-24 17:29:03 -0600

    fix server render

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Servidor funcionando 🔥");
+app.use(express.json());
+app.use(express.static('public'));
+
+// 🔥 prueba
+app.get('/ping', (req, res) => {
+  res.send("pong 🔥");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log("Servidor corriendo");
+  console.log("Servidor corriendo en puerto", PORT);
 });
