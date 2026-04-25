dd4ee0dc4ac2895d331949935aff8cb49e71017f
Gilberto <gil.martinez.meji@gmail.com>
2026-04-25 10:55:05 -0600

    fix static index render

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // =============================
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+
 app.use("/pdfs", express.static(path.join(__dirname, "pdfs")));
 
 // =============================
@@ -283,7 +283,7 @@ app.get("/", (req, res) => {
 });
 
 // 🔥 DESPUÉS static (solo UNA vez)
-app.use(express.static("public"));
+app.use(express.static("public" { index: false }));));
 
 // 🔥 LISTEN (NO LO BORRES)
 const PORT = process.env.PORT || 3000;
